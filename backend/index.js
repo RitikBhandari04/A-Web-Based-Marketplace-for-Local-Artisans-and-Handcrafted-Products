56198809a6f4d93cf890391e3afdda91483de3fc
RitikBhandari04 <bhandariritik825@gmail.com>
2026-03-30 14:59:50 +0530

    login backend done

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,11 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors")
 const dns = require("dns");
+const { type } = require("os");
+const bcrypt = require('bcryptjs')
 app.use(express.json());
 require('dotenv').config();
+
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 app.use(cors());
@@ -128,6 +131,111 @@ app.get("/allproducts",async (req,res)=>{
     console.log("All Products Fetched");
     res.send(products);
 })
+
+//User Scehma
+
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String
+    },
+    cartData:{
+        type:Object
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+// Creating Endpoint fro registering the user
+
+
+app.post('/signup',async(req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"});
+    }
+    let cart = {};
+
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+    }
+    const hashpass=bcrypt.hashSync(req.body.password,10);
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:hashpass,
+        cartData:cart,
+    })
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+
+    res.json({success:true,token});
+})
+
+//creating endpoint for user login
+
+app.post('/login', async (req, res) => {
+    try {
+        let user = await Users.findOne({ email: req.body.email });
+
+        if (user) {
+            const passCompare = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+
+            if (passCompare) {
+                const data = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                const token = jwt.sign(data, 'secret_ecom');
+
+                res.json({
+                    success: true,
+                    token
+                });
+            } else {
+                res.json({
+                    success: false,
+                    errors: "Wrong Password"
+                });
+            }
+
+        } else {
+            res.json({
+                success: false,
+                errors: "Wrong Email Address"
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            error: "Internal Server Error"
+        });
+    }
+});
 
 app.listen(port,(error)=>{
     if(!error){
